5a9a47bcd0256560feb7d2095248fccc99e5ca17
mgoode180566 <michaelgoode66@googlemail.com>
2025-11-22 21:14:43 +0000

    added CORS

--- src/main/java/com/example/vbolaps/config/CorsConfig.java
+++ src/main/java/com/example/vbolaps/config/CorsConfig.java
@@ -14,10 +14,9 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/**")
-					.allowedOrigins(
-						"https://master.d2xn66setsnq9o.amplifyapp.com",
-						"https://api.datalogging.click"
-					)
+					.allowedOrigins("https://master.d2xn66setsnq9o.amplifyapp.com",
+						"https://datalogging.click",
+						"https://www.datalogging.click")
 					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 					.allowedHeaders("*")
 					.allowCredentials(true);
