8dd5d6843ff57afba60fecd5a2979b54d97b2566
mgoode180566 <michaelgoode66@googlemail.com>
2025-12-03 14:16:09 +0000

    added logger to import

--- src/main/java/com/example/vbolaps/web/ImportController.java
+++ src/main/java/com/example/vbolaps/web/ImportController.java
@@ -25,10 +25,11 @@ public class ImportController {
     }
 
     @PostMapping(value="/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public Map<String, Object> importVbo(@RequestPart("file") MultipartFile file, @RequestPart("session") SessionDto sessionDto) throws Exception {
+    public String importVbo(@RequestPart("file") MultipartFile file, @RequestPart("session") SessionDto sessionDto) throws Exception {
         log.info("Received file: {}", file.getName());
-        Session session = importService.importVbo(file.getInputStream(), sessionDto);
-        log.info("Session lap count={}",session.getLaps().size());
-        return Map.of("sessionId", session.getId(), "circuit", session.getCircuit(), "driver", session.getDriver());
+        importService.importVbo(file.getInputStream(), sessionDto);
+        return "Completed";
+        //log.info("Session lap count={}",session.getLaps().size());
+        //return Map.of("sessionId", session.getId(), "circuit", session.getCircuit(), "driver", session.getDriver());
     }
 }
